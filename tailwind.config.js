d1b633d1f7d07d014ab6c4c7ec443583ea09e3fe
Siku Mahayi <smahayi31@gmail.com>
2022-09-06 23:48:35 +0200

    Initial commit, featuirng tailwind and node

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: ["./dist/*.html"],
+    darkMode: false,
+    theme: { extend: {} },
+    variants: { extend: {} },
+    plugins: [],
+};
